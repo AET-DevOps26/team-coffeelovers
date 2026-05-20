ad7d39d664f4688cccf49597aff55e53e20a4bf5
Adnan Fidan <addfinnn@gmail.com>
2026-05-20 16:18:45 +0200

    add common classes

---
+++ backend/common/src/main/java/coffeelovers/common/exception/BaseException.java
@@ -0,0 +1,21 @@
+package coffeelovers.common.exception;
+
+public class BaseException extends RuntimeException {
+
+    private final String code;
+    private final int statusCode;
+
+    public BaseException(String code, String message, int statusCode) {
+        super(message);
+        this.code = code;
+        this.statusCode = statusCode;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
